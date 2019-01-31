ad53c7c6cd6dcba9610a797eb3516009530c086a
Andrii Mutylo <amutylo@webcodin.com>
2019-01-31 18:13:01 +0200

    added connection to the mlab mongodb database

--- lib/app.ts
+++ lib/app.ts
@@ -1,14 +1,17 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
-import { Routes } from '../crm/routes/crmRoutes';
+import { Routes } from '../lib/routes/crmRoutes';
+import * as mongoose from 'mongoose';
 
 class App {
-    public app: express.Application;
+    public app: express.Application = express();
     public routePrv: Routes = new Routes();
+    public mongoUrl: string =
+        'mongodb://typenode:CP002360@ds119548.mlab.com:19548/typenode';
 
     constructor() {
-        this.app = express();
         this.config();
+        this.mongoSetup();
         this.routePrv.routes(this.app);
     }
 
@@ -17,6 +20,11 @@ class App {
         this.app.use(bodyParser.json());
         //support application/x-www-form-urlencoded post data
         this.app.use(bodyParser.urlencoded({ extended: false }));
+    }
+
+    private mongoSetup(): void {
+        mongoose.Promise = global.Promise;
+        mongoose.connect(this.mongoUrl);
     }
 }
 
