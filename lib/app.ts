4eec63d0c933af620fd65d744bc0460ba193abec
Andrii Mutylo <amutylo@webcodin.com>
2019-02-01 10:34:11 +0200

    remove mongodbuser creds

--- lib/app.ts
+++ lib/app.ts
@@ -4,28 +4,27 @@ import { Routes } from '../lib/routes/crmRoutes';
 import * as mongoose from 'mongoose';
 
 class App {
-    public app: express.Application = express();
-    public routePrv: Routes = new Routes();
-    public mongoUrl: string =
-        'mongodb://typenode:CP002360@ds119548.mlab.com:19548/typenode';
+	public app: express.Application = express();
+	public routePrv: Routes = new Routes();
+	public mongoUrl: string = 'mongodb://<mongodbuser>:<mingodbpass>@ds119548.mlab.com:19548/typenode';
 
-    constructor() {
-        this.config();
-        this.mongoSetup();
-        this.routePrv.routes(this.app);
-    }
+	constructor() {
+		this.config();
+		this.mongoSetup();
+		this.routePrv.routes(this.app);
+	}
 
-    private config(): void {
-        // support application/json type post data
-        this.app.use(bodyParser.json());
-        //support application/x-www-form-urlencoded post data
-        this.app.use(bodyParser.urlencoded({ extended: false }));
-    }
+	private config(): void {
+		// support application/json type post data
+		this.app.use(bodyParser.json());
+		//support application/x-www-form-urlencoded post data
+		this.app.use(bodyParser.urlencoded({ extended: false }));
+	}
 
-    private mongoSetup(): void {
-        mongoose.Promise = global.Promise;
-        mongoose.connect(this.mongoUrl);
-    }
+	private mongoSetup(): void {
+		mongoose.Promise = global.Promise;
+		mongoose.connect(this.mongoUrl);
+	}
 }
 
 export default new App().app;
